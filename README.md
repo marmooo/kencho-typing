0445de72aa4b769c44ac365ed4d989be780e5fae
marmooo <marmooo@users.noreply.github.com>
2025-07-27 07:53:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# 県庁所在地タイピング
+
+[日本の県庁所在地とタイピングを同時に学ぶアプリ](https://marmooo.github.io/kencho-typing/)です。
+
+## Build
+
+- `bash build.sh`
+
+## Attribution
+
+- `src/map.svg` is cloned from
+  [marmooo/japanese-prefectures](https://github.com/marmooo/japanese-prefectures)
+  licensed under the GFDL
+- `src/favicon/favicon.svg` is created from
+  [Emoji Two](https://github.com/EmojiTwo/emojitwo) licensed under the
+  Apache-2.0
---
+++ src/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "2025-07-27 00:00";
+const urlsToCache = [
+  "/prefectures-typing/",
+  "/prefectures-typing/index.js",
+  "/prefectures-typing/problems.csv",
+  "/prefectures-typing/mp3/bgm.mp3",
+  "/prefectures-typing/mp3/cat.mp3",
+  "/prefectures-typing/mp3/correct.mp3",
+  "/prefectures-typing/mp3/end.mp3",
+  "/prefectures-typing/mp3/keyboard.mp3",
+  "/prefectures-typing/favicon/favicon.svg",
+  "https://marmooo.github.io/fonts/textar-light.woff2",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
+      );
+    }),
+  );
+});
